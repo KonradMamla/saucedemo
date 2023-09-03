00829d15318fecd36e4bab2369eff44cc064d719
Konrad Mamka <konrad.mamla@gmail.com>
2023-09-03 21:21:32 +0200

    README file formating.

---
+++ cypress/e2e/Login/login.spec.ts
@@ -0,0 +1,65 @@
+import { LoginPage } from '../../PageObjects/LoginPage';
+import { User } from '../../support/types/users';
+
+describe('Login functionality', () => {
+
+  let standard_user: User;
+  let locked_out_user: User;
+  let problem_user: User;
+  let performance_glitch_user: User;
+  let loginPage: LoginPage;
+
+  before(() => {
+    cy.fixture('users.json').then((users) => {
+      standard_user = users.users[0];
+      locked_out_user = users.users[1];
+      problem_user = users.users[2];
+      performance_glitch_user = users.users[3];
+    });
+  });
+
+  beforeEach(() => {
+    loginPage = new LoginPage();
+    loginPage.navigationLoginPage();
+  })
+
+  it('Should log in with valid credentials', () => {
+    loginPage.fillUsername().type(standard_user.userName);
+
+    loginPage.fillPassword().type(standard_user.password);
+
+    loginPage.submitLoginForm();
+
+    cy.url().should('include', '/inventory.html');
+
+    cy.get('.title').should('have.text', 'Products');
+  });
+
+  it('Should not be able to login with invalid credentials', () => {
+
+    loginPage.fillUsername().type('invalid-username')
+
+    loginPage.fillPassword().type('invalid-password')
+
+    loginPage.submitLoginForm();
+
+    cy.get('[data-test=error]')
+      .should('be.visible')
+      .and('have.text', 'Epic sadface: Username and password do not match any user in this service')
+  })
+
+  it('Should not be able to log in with locked out user account', () => {
+
+    loginPage.fillUsername().type(locked_out_user.userName);
+
+    loginPage.fillPassword().type(locked_out_user.password);
+
+    loginPage.submitLoginForm();
+
+    cy.get('h3')
+    .should('be.visible')
+    .and('have.text', 'Epic sadface: Sorry, this user has been locked out.');
+  });
+
+
+});
